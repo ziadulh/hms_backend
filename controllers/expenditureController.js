a5925f0c732f0d7da00d31a38ab9181e3d6cb377
ziadul <mdzhhridoy@gmail.com>
2023-07-17 13:09:23 +0600

    Expentiture added

--- controllers/expenditureController.js
+++ controllers/expenditureController.js
@@ -1,8 +1,11 @@
 // const { Op, literal } = require('sequelize');
 // const Meal = require('../models/Meal');
 // const User = require('../models/User');
+// const { validationResult } = require('express-validator');
+// const express = require('express')
 
 const Expenditure = require("../models/Expenditure");
+const User = require("../models/User");
 
 // Meal.belongsTo(User, { foreignKey: 'userId' });
 
@@ -12,11 +15,37 @@ const myController = {
     index: async (req, res) => {
         try {
             const expenditures = await Expenditure.findAll();
+            // const users = await User.findAll();
+            // return res.status(200).json({ status: true, success: [{ msg: "All user info retrived" }], expenditures, users });
             return res.status(200).json({ status: true, success: [{ msg: "All user info retrived" }], expenditures });
         } catch (error) {
             return res.status(400).json({ status: false, errors: [{ msg: "Oops! Something Went Wrong" }], error });
         }
     },
+    // create Expenditure
+    create: async (req, res) => {
+        try {
+            // storing user data
+            const expenditure = await Expenditure.create({
+                name: req.body.name,
+                cost: req.body.cost,
+                status: req.body.status,
+                createdBy: req.user.id
+            });
+            return res.status(200).json({ status: true, success: [{ msg: "Expenditure Successfully Added" }], expenditure });
+        } catch (error) {
+            return res.status(400).json({ status: false, errors: [{ msg: "Oops! Something Went Wrong" }], error });
+        }
+    },
+    // get a Expenditure
+    // index: async (req, res) => {
+    //     try {
+    //         const expenditures = await Expenditure.findAll();
+    //         return res.status(200).json({ status: true, success: [{ msg: "All user info retrived" }], expenditures });
+    //     } catch (error) {
+    //         return res.status(400).json({ status: false, errors: [{ msg: "Oops! Something Went Wrong" }], error });
+    //     }
+    // },
 }
 
 module.exports = myController
--- routes/expenditure.js
+++ routes/expenditure.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const router = express.Router();
-// const { body } = require('express-validator');
+const { body } = require('express-validator');
 const authUser = require('../middlewares/authUser')
 const expenditureController = require('../controllers/expenditureController');
+// const { validationResult } = require('express-validator');
+
+// validation section
+const createValidation = [
+    body('cost')
+    .isLength({ min: 2, max: 50 })
+    .withMessage('cost should be at least 5 chars and at most 50 chars'),
+    
+    body('name').isLength({ min: 3, max: 50 })
+    .withMessage('name should be at least 3 chars and at most 50 chars'),
+
+    body('status').isIn(['active', 'inactive']).withMessage('status should be in (super, admin, user)')
+];
 
 
 // router section
-router.get('/',  expenditureController.index); // to get all user
-// router.post('/create', authUser, mealController.create); // to get all user
+router.get('/',  expenditureController.index); // to get expenditures
+router.post('/create', authUser, expenditureController.create); // to create expenditure
 
 
 
